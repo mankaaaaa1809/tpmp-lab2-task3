16f2f42e890ca7b8e593b028f7a2c0df0ae56b22
Lina <mankewi4.anatoly@gmail.com>
2026-02-28 14:01:05 +0300

    added directories

---
+++ include/abonent.h
@@ -0,0 +1,56 @@
+#ifndef ABONENT_H
+#define ABONENT_H
+
+/*structure and function prototypes used in main.c client,
+implementation can be found in abonent.c*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_ABONENTS 12
+#define MAX_TARIFF_LEN 20
+
+//connection date structure
+struct Date {
+	int year;
+	int month;
+	int day;
+};
+
+//main structure
+struct ABON1 {
+	char fio[50];
+	char phone[15];
+	struct Date connect_date;
+	char tariff[MAX_TARIFF_LEN];
+	float accrued;
+	float balance;
+};
+
+//function for inputting abonents data through console
+int input_abonents(struct ABON1 abonents[], int max_size);
+
+//function for comparing abonents by first letters
+int compare_abonents(const void *a, const void *b);
+
+//function for sorting abonents in alphabet order
+void sort_abonents(struct ABON1 abonents[], int count);
+
+//function for determining whether 10 years passed
+int is_more_than_10_years(struct Date connect_date);
+
+//function for adding 10 rub on abonent’s account
+void add_bonus(struct ABON1 abonents[], int count);
+
+//function for outputting abonents with negative balance
+void print_negative_balance(struct ABON1 abonents[], int count);
+
+//function for outputting number of abonents for each tariff
+void print_tariff_stats(struct ABON1 abonents[], int count);
+
+//function for outputting all the bonnets
+void print_all_abonents(struct ABON1 abonents[], int count);
+
+#endif
